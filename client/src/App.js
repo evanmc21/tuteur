b1fc52bcbb724cbf5c24a974e0004b7799d382a9
evanmc21 <emcandler21@gmail.com>
2018-09-22 20:53:51 -0400

    created client login form and function to send a token

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,23 @@ class App extends Component {
     this.state = {clientsReceived: ""}
     this.getClients = this.getClients.bind(this)
   }
+
+  login() {
+    const email = $("#email").value()
+    const password = $("#password").value()
+    const request = {"auth": {"email": email, "password": password}}
+    console.log(request)
+    $.ajax({
+      url: "http://localhost:3000/api/user_token",
+      type: "POST",
+      data: request,
+      dataType: "json",
+      success: function (result) {
+        console.log(result)
+        localStorage.setItem("jwt", result.jwt)
+      }
+    })
+  }
   getClients(){
     $.ajax({
       url: "http://localhost:3000/api/clients",
@@ -21,6 +38,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1 style={{marginTop: "20vh", marginBottom: "5vh"}}>
+          Banana Management System
+        </h1>
+        <form>
+          <label htmlFor="email">Email: </label>
+          <br />
+          <input
+            name="email"
+            id="email"
+            type="email"
+          />
+          <br /><br />
+          <label htmlFor="password">Password:</label>
+          <br />
+          <input
+            name="password"
+            id="password"
+            type="password"
+          />
+          </form>
+          <br />
+          <button
+            onClick={this.login}
+          >
+              Login
+          </button>
+        <br />
         <button
         onClick={this.getClients}
         style={{marginTop: '25vh'}}
