4901f32f2f07794d0130474d1b9b92f17317336e
evanmc21 <emcandler21@gmail.com>
2018-09-24 10:34:58 -0400

    profile data rendering on dashboard component

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import Auth from './modules/Auth';
 
 class Dashboard extends Component {
+  constructor(){
+    super();
+    this.state = {
+      myClients: null,
+      clientsReceived: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('/profile', {
+      method: "GET",
+      headers: {
+        token: Auth.getToken(),
+        'Authorization': `Token ${Auth.getToken()}`
+      }
+    }).then(res => res.json())
+      .then(res => {
+        this.setState({
+          myClients: res.clients,
+          clientsReceived: true
+      })
+    }).catch(err => console.log(err));
+  }
 
   render(){
     return(
       <div>
-        <h2>Beyonce</h2>
+        {(this.state.clientsReceived) ? this.state.myClients.map(client => {
+          return <h1 key={client.id}>{client.name}</h1>
+        })
+      : <p>loading..</p>}
       </div>
     )
   }
