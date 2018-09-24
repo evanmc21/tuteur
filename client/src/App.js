3505564c8b6b2aeb768add98cf9d44564cba700b
evanmc21 <emcandler21@gmail.com>
2018-09-24 02:12:21 -0400

    finally rendering client list

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import Login from './Login';
+// import Login from './Login';
 import Clients from './Clients';
-import { BrowserRouter as Router, Link, Redirect, Route}
+import { BrowserRouter as Router, Link, Redirect, Route } from 'react-router-dom';
 import Auth from './modules/Auth';
-// import $ from 'jquery'
 import './App.css';
 // const API_URL = process.env.REACT_APP_API_URL;
 
@@ -23,12 +22,12 @@ class App extends Component {
       <h1 style={{marginTop: "20vh", marginBottom: "5vh"}}>
           Tuteur
         </h1>
+      <Route exact path="/clients" render={() =>
+      <Clients />} />
 
-        <br />
-        <Login />
-        <Clients />
 
       </div>
+      </Router>
     );
   }
 }
--- client/src/Clients.js
+++ client/src/Clients.js
@@ -1,106 +1,49 @@
 import React, { Component } from 'react';
-import Client from './Client';
-import NewClientForm from './NewClientForm';
-import $ from 'jquery';
-import EditClientForm from './EditClientForm';
+// import Client from './Client';
+// import NewClientForm from './NewClientForm';
+// import EditClientForm from './EditClientForm';
 // import ClientCard from './ClientCard';
-let token = "Bearer " + localStorage.getItem("jwt")
+// let token = "Bearer " + localStorage.getItem("jwt")
 
 class Clients extends Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
-      clients: [],
-      editingClientId: null
+      clientList: null,
+      clientsReceived: false,
     }
-    this.addNewClient = this.addNewClient.bind(this)
-    this.removeClient = this.removeClient.bind(this)
-    this.editingClient = this.editingClient.bind(this)
-    this.editClient = this.editClient.bind(this)
   }
 
-
-
-  componentsDidMount(){
-
-      $.ajax({
-        url: "http://localhost:3000/api/clients",
-        type: "GET",
-        beforeSend: function(xhr){xhr.setRequestHeader('Authorization', token)},
-        context: this,
-        success: function (result){
-          this.setState({clients: JSON.stringify(result)})
-        }
-      })
-
-    }
-
-    addNewClient(name, age, location, school, goals, notes, rate, userId){
-      $.ajax({
-        url: ("http://localhost:3000/api/clients", { client: {name: name, age, location, school, goals, notes, rate, userId }}),
-        type: "POST",
-        beforeSend: function(xhr){xhr.serRequestHeader('Authorization', token)},
-        context: this,
-        success: function (result){
-          const clients = [ ...this.state.clients, result.data]
-          this.setState({clients: JSON.stringify(clients)})
-        }
-      })
-    }
-
-    removeClient(id) {
-      $.ajax({
-        url:"http://localhost:3000/api/clients/" + id,
-        type: "DELETE",
-        beforeSend: function(xhr){xhr.serRequestHeader('Authorization', token)},
-        context: this,
-        success: function (result){
-          const clients = this.state.clients.filter(client => client.id !== id)
-          this.setState({clients})
-        }
-      })
-    }
-
-    editingClient(id) {
+componentDidMount(){
+  fetch('/clients')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
       this.setState({
-        editingClientId: id
+        clientList: res.clients,
+        clientsReceived: true,
       })
-    }
+    }).catch(err => console.log(err));
+  }
 
-    editClient(id, name, age, location, school, goals, notes, rate, userId) {
-      $.ajax({
-        url:"http://localhost:3000/api/clients/" + id,
-        method: "PATCH",
-        beforeSend: function(xhr){xhr.serRequestHeader('Authorization', token)},
-        context: this,
-        success: function (result){
-          const clients = this.state.clients;
-          clients[id-1] = {id, name, age, location, school, goals, notes, rate, userId}
-          this.setState(() =>({
-            clients,
-            editingClientId: null
-          }))
-        }
-      })
-    }
+    renderClients(){
+        return this.state.clientList.map(client => {
+          return(
+            <div className="client" key={client.id}>
+              <h1>{client.name}</h1>
+              <p>{client.age}</p>
+            </div>
+          )
+        })
+      }
 
-      render(){
-        return (
-        <div>
-            {this.state.clients.map(client => {
-              if (this.state.editingClientId === client.id ){
-                return (<EditClientForm client={client} key={client.id} editClient={this.editClient} />)
-              } else {
-                return (<Client client={client}
-                  key={client.id}
-                  onRemoveClient={this.removeClient}
-                  editingClient={this.editingClient} />)
-              }
-            })}
-            <NewClientForm onNewClient={this.addNewClient} />
+    render(){
+      return (
+        <div className="client-list">
+          {(this.state.clientsReceived)
+          ? this.renderClients()
+          : <p>hold up...</p>}
         </div>
-
-
       )
     }
 }
