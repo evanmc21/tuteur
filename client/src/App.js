890689d1569d4ea1b29651e32037e6f9159106e2
evanmc21 <emcandler21@gmail.com>
2018-09-23 22:43:40 -0400

    created auth module and getting rid of jQuery in requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import Login from './Login';
 import Clients from './Clients';
+import { BrowserRouter as Router, Link, Redirect, Route}
+import Auth from './modules/Auth';
 // import $ from 'jquery'
 import './App.css';
 // const API_URL = process.env.REACT_APP_API_URL;
 
 class App extends Component {
-
-
-
+  constructor(){
+    super();
+    this.state = {
+      auth: Auth.isUserAuthenticated(),
+    }
+  }
 
 
   render() {
     return (
+      <Router>
       <div className="App">
       <h1 style={{marginTop: "20vh", marginBottom: "5vh"}}>
           Tuteur
