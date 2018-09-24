df8002781547caf1ca03a6c4446b007ea050cb6b
evanmc21 <emcandler21@gmail.com>
2018-09-24 17:32:17 -0400

    refactored client form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -16,7 +16,6 @@ class ClientsController < ApplicationController
   end
 
   def create
-    binding.pry
     client = Client.new(client_params)
     client.user = current_user
     if client.save
--- client/src/NewClientForm.js
+++ client/src/NewClientForm.js
@@ -5,13 +5,13 @@ class NewClientForm extends Component {
     constructor() {
       super();
       this.state = {
-        name: '',
-        age: '',
-        location: '',
-        school: '',
-        goals: '',
-        notes: '',
-        rate: ''
+          name: '',
+          age: 0,
+          location: '',
+          school: '',
+          goals: '',
+          notes: '',
+          rate: 0
       }
       this.handleChange = this.handleChange.bind(this)
     }
@@ -26,51 +26,53 @@ class NewClientForm extends Component {
 
     render() {
       return(
-        <form onSubmit={(e) => this.props.addClient(e, this.state)}>
-            <input
-                type="text"
-                name="name"
-                placeholder="Name" required
-                value={this.state.name}
-                onChange={this.handleChange} />
-            <input
-                type="number"
-                name="age"
-                placeholder="Age" required
-                value={this.state.age}
-                onChange={this.handleChange} />
-            <input
-                type="text"
-                name="location"
-                placeholder="Location" required
-                value={this.state.location}
-                onChange={this.handleChange}/>
-            <input
-                type="text"
-                name="school"
-                placeholder="School" required
-                value={this.state.school}
-                onChange={this.handleChange}/>
-            <input
-                type="text"
-                name="goals"
-                placeholder="Goals"
-                value={this.state.goals}
-                onChange={this.handleChange}  />
-            <input
-                type="text"
-                name="notes"
-                placeholder="Notes"
-                value={this.state.notes}
-                onChange={this.handleChange}  />
-            <input
-                type="number"
-                name="rate"
-                placeholder="Rate per hour" required
-                value={this.state.rate}
-                onChange={this.handleChange} />
-                <input type="submit" value="add client" />
-            </form>
+        <div>
+          <form onSubmit={(e) => this.props.addClient(e, this.state)}>
+              <input
+                  type="text"
+                  name="name"
+                  placeholder="Name" required
+                  value={this.state.name}
+                  onChange={this.handleChange} />
+              <input
+                  type="number"
+                  name="age"
+                  placeholder="Age" required
+                  value={this.state.age}
+                  onChange={this.handleChange} />
+              <input
+                  type="text"
+                  name="location"
+                  placeholder="Location" required
+                  value={this.state.location}
+                  onChange={this.handleChange}/>
+              <input
+                  type="text"
+                  name="school"
+                  placeholder="School" required
+                  value={this.state.school}
+                  onChange={this.handleChange}/>
+              <input
+                  type="text"
+                  name="goals"
+                  placeholder="Goals"
+                  value={this.state.goals}
+                  onChange={this.handleChange}  />
+              <input
+                  type="text"
+                  name="notes"
+                  placeholder="Notes"
+                  value={this.state.notes}
+                  onChange={this.handleChange}  />
+              <input
+                  type="number"
+                  name="rate"
+                  placeholder="Rate per hour" required
+                  value={this.state.rate}
+                  onChange={this.handleChange} />
+                  <input type="submit" value="add client" />
+              </form>
+            </div>
   )}
 }
 
