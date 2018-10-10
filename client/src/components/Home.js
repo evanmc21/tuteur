12b93879391df25e8467d36c628f8142469881ab
evanmc21 <emcandler21@gmail.com>
2018-10-09 23:34:39 -0400

    images now displayed on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Container, Row, Col, Jumbotron } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import tutor1 from '../images/tutor1.jpg';
+import tutor2 from '../images/tutor2.jpg';
+import tutor3 from '../images/tutor3.jpg';
+
 import '../App.css'
 const Home = () => {
   return (
+    <div className="container-flex">
     <Jumbotron id="jumbotron" fluid>
-    <Container fluid="fluid">
+    <Container id="container-home" fluid="fluid">
       <div className="home">
           <h1 id="home-display">TUTEUR</h1>
           <Link id="login" to="/login">LOGIN</Link>
         </div>
   </Container>
 </Jumbotron>
-)
+<img src={tutor1} alt="" />
+<img src={tutor2} alt="" />
+<img src={tutor3} alt="" />
+</div>
+);
 }
 
 export default Home;
